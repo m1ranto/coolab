14c980366acf137d99b2368e3277de177afee570
Miranto <85173375+mirantocodes@users.noreply.github.com>
2023-02-07 08:56:32 +0300

    Improve Collaborator

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,2 +1,4 @@
 class Collaborator < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :email
 end
--- test/models/collaborator_test.rb
+++ test/models/collaborator_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class CollaboratorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @collaborator = Collaborator.new(name: "bob", email: "bob@email.com")
+  end
+
+  test "should be valid" do
+    assert @collaborator.valid?
+  end
+
+  test "name should be present" do
+    @collaborator.name = ""
+    assert_not @collaborator.valid?
+  end
+
+  test "email should be present" do
+    @collaborator.email = ""
+    assert_not @collaborator.valid?
+  end
 end
