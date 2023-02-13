ee6c5edc5ffc085219d8ff46a38ba211e02d0ad4
Miranto <85173375+mirantocodes@users.noreply.github.com>
2023-02-13 14:44:24 +0300

    Improve collaborator validations

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,6 +1,6 @@
 class Collaborator < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :email
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }, uniqueness: true
 
   has_secure_password
 end
