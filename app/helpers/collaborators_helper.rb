e7444cf6913f08f6c7a70fd058fb08c253b69f10
Miranto <85173375+mirantocodes@users.noreply.github.com>
2023-02-24 08:14:22 +0300

    Refactor profile picture codes

--- app/helpers/collaborators_helper.rb
+++ app/helpers/collaborators_helper.rb
@@ -1,19 +1,27 @@
 module CollaboratorsHelper
   # Default profile with first word of collaborator name
-  def default_profile(collaborator)
-    collaborator.name[0]
-    content_tag(:figure, collaborator.name[0], class: "default-profile mb-0", style: "background-color: #{collaborator.profile}")
+  def profile_picture(collaborator)
+    profile_picture_size(collaborator)
   end
 
   # Default large profile with first word of collaborator name
-  def default_large_profile(collaborator)
-    collaborator.name[0]
-    content_tag(:figure, collaborator.name[0], class: "default-profile-lg fs-1 mb-0", style: "background-color: #{collaborator.profile}")
+  def profile_picture_large(collaborator)
+    profile_picture_size(collaborator, class_size: "profile-picture profile-picture-lg")
   end
 
   # Default small profile with first word of collaborator name
-  def default_small_profile(collaborator)
-    collaborator.name[0]
-    content_tag(:figure, collaborator.name[0], class: "default-profile-sm small mb-0", style: "background-color: #{collaborator.profile}")
+  def profile_picture_small(collaborator)
+    profile_picture_size(collaborator, class_size: "profile-picture profile-picture-sm")
   end
+
+  private
+    # Get first letter of collaborator name
+    def first_letter(collaborator)
+      collaborator.name[0]
+    end
+
+    # Return size of profile picture
+    def profile_picture_size(collaborator, class_size: "profile-picture")
+      content_tag(:figure, first_letter(collaborator), class: class_size, style: "background-color: #{collaborator.profile}")
+    end
 end
