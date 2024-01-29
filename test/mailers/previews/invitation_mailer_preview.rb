c56c579d0bc5844a74158094f31f8a1564dc5515
Miranto <miranto.hhs@gmail.com>
2024-01-29 11:38:39 +0300

    Remove minitest test/ directory

--- test/mailers/previews/invitation_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
-class InvitationMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/invitation_mailer/invite_collaborator
-  def invite_collaborator
-    InvitationMailer.invite_collaborator "to@example.org"
-  end
-
-end
