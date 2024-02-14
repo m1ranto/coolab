17bcb50a455d81b0db739729e564d1a798748568
Miranto <miranto.hhs@gmail.com>
2024-02-14 16:16:14 +0300

    Pin slimselect.js

---
+++ app/javascript/controllers/slim_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import SlimSelect from 'slim-select'
+
+export default class extends Controller {
+  connect() {
+    new SlimSelect({
+      select: this.element,
+      settings: {
+        placeholderText: 'Select collaborator',
+      }
+    })
+  }
+}
