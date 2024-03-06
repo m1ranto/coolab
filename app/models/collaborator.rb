607aa7a10b503b55b16b66710b41ffc0bc5f48b1
Miranto <miranto.hhs@gmail.com>
2024-03-06 15:18:51 +0300

    Validate file upload

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -8,6 +8,7 @@ class Collaborator < ApplicationRecord
                     length: { maximum: 50 },
                     uniqueness: true,
                     format: { with: VALID_EMAIL_REGEX }
+  validate :valid_profile_picture
 
   has_many :projects, dependent: :destroy
   has_and_belongs_to_many :todos
@@ -23,4 +24,23 @@ class Collaborator < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  private
+
+    def valid_profile_picture
+      limit_size = 1050000
+      if profile_picture.attached?
+        if profile_picture.blob.byte_size > limit_size && !profile_picture.blob.content_type.starts_with?('image/')
+          profile_picture.purge
+          errors.add(:base, "Too big file, maximum is 1 MB")
+          errors.add(:base, "Must be an image")
+        elsif profile_picture.blob.byte_size > limit_size
+          profile_picture.purge
+          errors.add(:base, "Too big file, maximum is 1 MB")
+        elsif !profile_picture.blob.content_type.starts_with?('image/')
+          profile_picture.purge
+          errors.add(:base, "Must be an image")
+        end
+      end
+    end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,21 @@
 class Document < ApplicationRecord
   validates :name, presence: true
+  validate  :files_size
 
   belongs_to :project
   belongs_to :collaborator, optional: true
   has_many_attached :files
+
+  private
+
+    def files_size
+      if files.attached?
+        files.each do |file|
+          if file.blob.byte_size > 1050000
+            file.purge
+            errors.add(:base, "Too big file, maximum is 1 MB")
+          end
+        end
+      end
+    end
 end
