1e9200904b9590a5c25af730d458b658d6613c4d
Miranto <miranto.hhs@gmail.com>
2024-03-08 16:59:04 +0300

    Show flash for copy clipboard

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "source" ]
+  static targets = [ "source", "flash" ]
 
   copy() {
+    // copy to clipboard
     navigator.clipboard.writeText(this.sourceTarget.value)
+
+    // show flash
+    this.flashTarget.innerHTML = `
+      <section class="card-darker flash shadow-lg" data-controller="flash">
+        <h3 class="fs-6 mb-0">
+          <i class="bi bi-check-circle-fill text-success me-1"></i>
+          Invite link copied to clipboard
+        </h3>
+      </section>
+    `
   }
 }
